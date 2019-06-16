651f56cfa8872d60b17bde8b9ea39aa1c1dcb742
Ashley Erffmeyer <erffmea@gmail.com>
2019-06-15 21:26:34 -0500

    pseudo code updates to .js file & script tags added to .html file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,13 +8,28 @@ var currentWordBlanksText = document.getElementById("current-word-blanks");
 var guessesText = document.getElementById("guesses");
 var guessedLettersText = document.getElementById("guesses-letters-list");
 
+//Create variables associated with words
+var friends = ['Ross', 'Rachel', 'Monica', 'Phoebe', 'Chandler', 'Joey', 'Marcel'];
+var individualFriends = "";
+var lettersInFriends = [];
+var blanksAndSuccesses = [];
+
+//Populates blanks and successes with right number of blanks
+for (var i = 0; i < friends.length; i++) {
+    // Logs the Friend l at index position i to the console. This code is executed each we go through the loop.
+    console.log(Friends[i]);
+}
+var individualChar = individualFriends.split("");
+
+
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
-
     // Determines which key was pressed.
     var userGuess = event.key;
-
 }
+
+//
+
 // Use a function similar to this to check if letter input by user matches letters of the current word blanks.
 function vowelChecker(x) {
     var firstChar = x.toLowerCase().charAt(0);
